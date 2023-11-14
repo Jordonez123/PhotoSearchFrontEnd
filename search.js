9b73db08482ccc274e75020dce41a27e8ac92c04
VGan <vedantgannu@gmail.com>
2023-11-14 08:18:56 -0500

    GET request code for search

--- search.js
+++ search.js
@@ -1,5 +1,17 @@
 function search() {
     console.log("what the user typed in: ", document.getElementById("fname").value)
     document.getElementById('resultDiv').innerHTML = document.getElementById("fname").value;
+
+    sdk.searchGet({
+      q: document.getElementById("fname").value
+    }, {}, {}).then((response) => {
+      
+      console.log(response);
+      var data = response.data;
+      console.log(data);
+    }).catch((error) => {
+      console.log('an error occurred', error);
+    });
+
     return
   }
